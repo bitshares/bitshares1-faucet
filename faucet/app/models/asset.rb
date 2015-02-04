2615806e7fb49b282d487584b80d852b5deb8682
valzav <i@valzav.com>
2015-02-04 10:38:06 -0500

    few minor fixes

--- faucet/app/models/asset.rb
+++ faucet/app/models/asset.rb
@@ -3,7 +3,7 @@ class Asset < ActiveRecord::Base
 
   def self.sync_with_blockchain
     counter = 0
-    data = BitShares::API::Blockchain.list_assets()
+    data = BitShares::API::Blockchain.list_assets("", -1)
     data.each do |a|
       next if Asset.exists?(assetid: a['id'])
       Asset.create(assetid: a['id'], symbol: a['symbol'], name: a['name'], description: a['description'][0..240], precision: a['precision'])
--- faucet/db/seeds.rb
+++ faucet/db/seeds.rb
@@ -1,3 +1,3 @@
 Asset.sync_with_blockchain
 
-User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+User.create!(name: 'admin', email: 'admin@example.com', password: 'password', password_confirmation: 'password')
