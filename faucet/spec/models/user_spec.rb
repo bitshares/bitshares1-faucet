61f18aba9a463544de9765d981943cec6e47b040
Dmitry Pak <fenec230@yahoo.com>
2015-02-08 14:55:45 +0700

    Add new tests

--- faucet/spec/models/user_spec.rb
+++ faucet/spec/models/user_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 describe User do
-  let(:user) { create :user }
+  #let(:user) { create :user }
+
+  describe "#find_for_oauth" do
+    it "should confirm real email" do
+      auth = OmniAuth::AuthHash.new(
+          {"provider" => "linkedin", "uid" => "123", "info" => {"name" => "Username", email: "mail@mail.com"}}
+      )
+      user = User.find_for_oauth(auth, nil, nil)
+
+      expect(user.email).to eq('mail@mail.com')
+      expect(user.confirmed?).to eq(true)
+    end
+  end
 
   describe "#after_confirmation" do
     it "should set subscription job" do
