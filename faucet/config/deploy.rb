88ae6a200dcc6258676d4be8bfda16a96709d207
Dmitry Pak <fenec230@yahoo.com>
2015-04-03 08:59:02 +0800

    Add capistrano

--- faucet/config/deploy.rb
+++ faucet/config/deploy.rb
@@ -1,28 +1,51 @@
-require 'mina/git'
-require 'mina/bundler'
-require 'mina/rails'
-require 'mina/rvm'
+# config valid only for current version of Capistrano
+lock '3.4.0'
 
-set :domain, 'faucet'
-set :user, 'bts'
-set :deploy_to, '/www'
-set :repository, 'https://github.com/BitShares/web_services.git'
-set :shared_paths, ['config/bitshares.yml', 'config/secrets.yml']
+set :application, 'faucet'
+set :repo_url, 'git@github.com:BitShares/web_services.git'
+set :repo_tree, '/faucet'
 
-task :environment do
-  invoke :'rvm:use[ruby-2.1.3@default]'
-end
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-task deploy: :environment do
-  deploy do
-    invoke :'git:clone'
-    #invoke :'deploy:link_shared_paths'
-    #invoke :'bundle:install'
-    #invoke :'rails:db_migrate'
-    #invoke :'rails:assets_precompile'
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/www/faucet'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+before 'deploy', 'rvm1:install:rvm'
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
   end
-end
 
-task :restart do
-  queue 'sudo service restart nginx'
 end
